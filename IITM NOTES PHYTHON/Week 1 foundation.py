84b57397651a9b26272b78c53f32335133263548
ak011125 <ajaykumarjakhar08@gmail.com>
2025-01-24 19:02:40 +0530

    IITM 1st week

---
+++ IITM NOTES PHYTHON/Week 1 foundation.py
@@ -0,0 +1,10 @@
+# Notes IITM Python lecture week 1
+# / always store in float
+# If you want to print single quotes, then enclose it within double quotes.
+print('1 2 3 4 5')
+print(1, 2, 3, 4, 5)
+# both 4 and 5 are same 
+
+w1 = input()
+w2 = input()
+print(w1 + w2) #if input is 10 ,20 then result is 1020 without int
